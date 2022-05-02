0d437067e3d9c269779bf1c1cd121e48b6d2409a
alexhscott <alexhenriscott@gmail.com>
2022-05-02 11:50:26 -0500

    modal on page load & redirect button

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,17 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('hey')
    
+    // Disables 'Pair with your ___' buttons on page load
     document.getElementById("meal-btn").disabled = true;
     document.getElementById("wine-btn").disabled = true;
     
-    var elems = document.getElementById('modal1');
-    var instances = M.Modal.getInstance(elems);
-    var instance = M.Modal.getInstance(elems);
-   
-    $('.modal').modal()
+    // Modal on page load
+    var modalElem = document.querySelector('.modal');
+    var instance = M.Modal.init(modalElem);
+    instance.open();
   });
 
-
  document.getElementById('yes-btn').addEventListener('click', event => {
-
+  console.log('you clicked yes')
  })
+
+ document.getElementById('no-btn').addEventListener('click', event => {
+   console.log('you clicked no')
+ })
+
+ function redirect() {
+  location.replace('https://consumer.ftc.gov/features/we-dont-serve-teens');
+ }
