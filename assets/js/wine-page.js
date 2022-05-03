1036f9d258ecdb7df99bf609d426e15d04c254c3
keithmw <keithmw5597@yahoo.com>
2022-05-02 21:01:37 -0500

    wine page update

--- assets/js/wine-page.js
+++ assets/js/wine-page.js
@@ -1,61 +1,61 @@
-var redWineSubmitButtonsEl = document.querySelector('#red-submit');
-var wine = document.querySelector('#wine-input');
+var cabernetSauvignon = document.getElementById("cabernet_sauvignon");
+var merlot = document.getElementById("merlot");
+var zinfandel = document.getElementById("zinfandel");
+var malbec = document.getElementById("malbec");
+var shiraz = document.getElementById("shiraz");
+var pinotNoir = document.getElementById("pinot_noir");
+var port = document.getElementById("port");
 
-var merlotPair = "https://api.spoonacular.com/food/wine/dishes?apiKey=e29f13492bd14f8bb491da8f76f9d4fe&wine=Merlot";
-var pairContainerEl = document.querySelector('#pair-container');
 
+var search = "https://api.spoonacular.com/food/wine/dishes?apiKey=e29f13492bd14f8bb491da8f76f9d4fe&wine=";
 
-console.log(Response);
+cabernetSauvignon.addEventListener("click", function() {
+    fetch(search+"cabernet_sauvignon")
+    .then(response => response.json())
+    .then(data => console.log(JSON.stringify(data)));
+    console.log("this works");
+});
 
+merlot.addEventListener("click",function() {
+    fetch(search+"merlot")
+    .then(response => response.json())
+    .then(data => console.log(JSON.stringify(data)));
+    console.log("this works");
+});
 
-//button handler
-var submitHandler = function (event) {
-  event.preventDefault();
-
-  var redWine = text.value.trim();
-
-  if (redWine) {
-    getWinePair(redWine);
-
-    pairContainerEl.textContent = '';
-    wine.value = '';
-  } else {
-    alert('Please enter a Wine to pair');
-  }
-  console.log(submitHandler);
-};
-
-//make request
-var getWinePair = function (wine) {
-  var winePairUrl = "https://api.spoonacular.com/food/wine/dishes?apiKey=e29f13492bd14f8bb491da8f76f9d4fe&wine=" + wine + '"';
-
-  fetch(winePairUrl)
-    .then(function (response) {
-      if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-          console.log(data);
-          displayRepos(data, wine);
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to connect to spoonacular');
-    });
-};
-// //display results
-// var displayPair = function () {
-//   if ( === 0) {
-//     pairContainerEl.textContent = '';
-//     return;
-//   }
+zinfandel.addEventListener("click",function() {
+    fetch(search+"zinfandel")
+    .then(response => response.json())
+    .then(data => console.log(JSON.stringify(data)));
+    console.log("this works");
+});
+shiraz.addEventListener("click",function() {
+    fetch(search+"shiraz")
+    .then(response => response.json())
+    .then(data => console.log(JSON.stringify(data)));
+    console.log("this works");
+});
+malbec.addEventListener("click",function() {
+    fetch(search+"malbec")
+    .then(response => response.json())
+    .then(data => console.log(JSON.stringify(data)));
+    console.log("this works");
+});
+pinotNoir.addEventListener("click",function() {
+  fetch(search+"pinot_noir")
+  .then(response => response.json())
+  .then(data => console.log(JSON.stringify(data)));
+  console.log("this works");
+});
+port.addEventListener("click",function() {
+  fetch(search+"port")
+  .then(response => response.json())
+  .then(data => console.log(JSON.stringify(data)));
+  console.log("this works");
+});
 
 
 
 
  
-//   }
-// };
-redWineSubmitButtonsEl.addEventListener('click', submitHandler);
+
