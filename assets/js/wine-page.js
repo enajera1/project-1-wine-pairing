56b995ea801b4f959fdbf83b8b22d987daf040eb
keithmw <keithmw5597@yahoo.com>
2022-05-02 19:37:55 -0500

    red-wine

--- assets/js/wine-page.js
+++ assets/js/wine-page.js
@@ -1,27 +1,61 @@
-// - **Type**: API key
-// - **API key parameter name**: apiKey
-// - **Location**: URL query string
+var redWineSubmitButtonsEl = document.querySelector('#red-submit');
+var wine = document.querySelector('#wine-input');
+
+var merlotPair = "https://api.spoonacular.com/food/wine/dishes?apiKey=e29f13492bd14f8bb491da8f76f9d4fe&wine=Merlot";
+var pairContainerEl = document.querySelector('#pair-container');
 
 
-// import SpoonacularApi from 'spoonacular_api';
-// let defaultClient = SpoonacularApi.ApiClient.instance;
-// // Configure API key authorization: apiKeyScheme
-// let apiKeyScheme = defaultClient.authentications['apiKeyScheme'];
-// apiKeyScheme.apiKey = 'e29f13492bd14f8bb491da8f76f9d4fe';
-// // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-// apiKeyScheme.apiKeyPrefix = 'Token';
+console.log(Response);
 
-// let apiInstance = new SpoonacularApi.WineApi();
-// let wine = malbec; // String | The type of wine that should be paired, e.g. \"merlot\", \"riesling\", or \"malbec\".
-// apiInstance.getDishPairingForWine(wine, (error, data, response) => {
-//   if (error) {
-//     console.error(error);
-//   } else {
-//     console.log('API called successfully. Returned data: ' + data);
-//   }
-// });
 
-var getWineData = function() {
-  fetch("https://api.spoonacular.com/food/wine/pairing?api_key=e29f13492bd14f8bb491da8f76f9d4fe");
+//button handler
+var submitHandler = function (event) {
+  event.preventDefault();
+
+  var redWine = text.value.trim();
+
+  if (redWine) {
+    getWinePair(redWine);
+
+    pairContainerEl.textContent = '';
+    wine.value = '';
+  } else {
+    alert('Please enter a Wine to pair');
+  }
+  console.log(submitHandler);
 };
-getWineData();
+
+//make request
+var getWinePair = function (wine) {
+  var winePairUrl = "https://api.spoonacular.com/food/wine/dishes?apiKey=e29f13492bd14f8bb491da8f76f9d4fe&wine=" + wine + '"';
+
+  fetch(winePairUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayRepos(data, wine);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to spoonacular');
+    });
+};
+// //display results
+// var displayPair = function () {
+//   if ( === 0) {
+//     pairContainerEl.textContent = '';
+//     return;
+//   }
+
+
+
+
+ 
+//   }
+// };
+redWineSubmitButtonsEl.addEventListener('click', submitHandler);
