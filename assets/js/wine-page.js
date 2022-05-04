f8e7aadc1d76cd6e2134a709601a1d21844bcfbb
kekehua <kenneth443641@gmail.com>
2022-05-04 16:06:54 -0500

    added search bar and buttons work

--- assets/js/wine-page.js
+++ assets/js/wine-page.js
@@ -2,7 +2,10 @@ var wineButtonsEl = document.querySelector("#wineR");
 var wineButtonsE2 = document.querySelector("#wineW");
 var repoContainerEl = document.querySelector('#repos-container');
 var mealText = document.querySelector(".text");
-var mealPairings = document.querySelector(".pairing");
+var mealPairings = document.querySelector(".pairings");
+var inputValue = document.querySelector('.inputValue');
+var button = document.querySelector('#input-btn');
+
 var buttonClickHandler = function (event) {
     var wine = event.target.getAttribute('wine-type');
   
@@ -25,6 +28,21 @@ var getFeaturedRepos = function (wine) {
       mealPairings.innerHTML = pairings;
     });
 };
+
+button.addEventListener('click',function(){
+  fetch("https://api.spoonacular.com/food/wine/dishes?wine="+inputValue.value+"&apiKey=17dacddd3e80476cad2abf4b81232653")
+  .then(response => response.json())
+  .then(data => {
+    var text = data["text"];
+    var pairings = data["pairings"];
+
+    mealText.innerHTML = text;
+    mealPairings.innerHTML = pairings;
+
+    inputValue.textContent='';
+  });
+  
+});
 
 wineButtonsEl.addEventListener("click",buttonClickHandler);
 wineButtonsE2.addEventListener("click",buttonClickHandler);
