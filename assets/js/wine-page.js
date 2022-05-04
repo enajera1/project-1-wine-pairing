b6f0a8324aea48c219f9235b786d7da395154440
kekehua <kenneth443641@gmail.com>
2022-05-04 15:32:23 -0500

    changes made except meal-page

--- assets/js/wine-page.js
+++ assets/js/wine-page.js
@@ -1,6 +1,8 @@
 var wineButtonsEl = document.querySelector("#wineR");
 var wineButtonsE2 = document.querySelector("#wineW");
-var repoContainerEl = document.querySelector('#repos-container')
+var repoContainerEl = document.querySelector('#repos-container');
+var mealText = document.querySelector(".text");
+var mealPairings = document.querySelector(".pairing");
 var buttonClickHandler = function (event) {
     var wine = event.target.getAttribute('wine-type');
   
@@ -11,11 +13,17 @@ var buttonClickHandler = function (event) {
     }
   };
 
-  var getFeaturedRepos = function (wine) {
+var getFeaturedRepos = function (wine) {
     var apiUrl = "https://api.spoonacular.com/food/wine/dishes?apiKey=17dacddd3e80476cad2abf4b81232653&wine=" + wine;
     fetch(apiUrl)
     .then(response => response.json())
-    .then(data => console.log((data)));
+    .then(data => {
+      var text = data["text"];
+      var pairings = data["pairings"];
+
+      mealText.innerHTML = text;
+      mealPairings.innerHTML = pairings;
+    });
 };
 
 
